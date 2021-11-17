c649d321c58fec4a246a936d8463683d2e6cfef7
Jaedson <jaedson33@gmail.com>
2021-11-16 21:00:33 -0300

    Optimized dictionary import.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n'
+import { register, init, getLocaleFromNavigator } from 'svelte-i18n'
 import App from './App.svelte'
 
-import en from './i18n/en.json'
-import pt from './i18n/pt.json'
-
-addMessages('en', en)
-addMessages('pt', pt)
+register('en', () => import('./i18n/en.json'))
+register('pt', () => import('./i18n/pt.json'))
 
 init({
   fallbackLocale: 'en',
   initialLocale: getLocaleFromNavigator(),
+}).then(() => {
+  const app = new App({ target: document.body })
+
+  if (import.meta.hot) {
+    import.meta.hot.accept()
+    import.meta.hot.dispose(() => app.$destroy())
+  }
+
+  if (__SNOWPACK_ENV__.NODE_ENV === 'production') {
+    navigator.serviceWorker.register('/sw.js')
+  }
 })
-
-const app = new App({ target: document.body })
-
-if (import.meta.hot) {
-  import.meta.hot.accept()
-  import.meta.hot.dispose(() => app.$destroy())
-}
-
-if (__SNOWPACK_ENV__.NODE_ENV === 'production') {
-  navigator.serviceWorker.register('/sw.js')
-}
