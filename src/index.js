0703aa331c8602a9e3716bc850ef4aecba24e16b
Jaedson <jaedson33@gmail.com>
2021-11-16 21:13:19 -0300

    Fixed error on dictionary fetch.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import { register, init, getLocaleFromNavigator } from 'svelte-i18n'
+import { version } from './settings'
 import App from './App.svelte'
 
-register('en', () => import('./i18n/en.json'))
-register('pt', () => import('./i18n/pt.json'))
+register('en', () => fetch('/dist/i18n/en.json').then(v => v.json()))
+register('pt', () => fetch('/dist/i18n/pt.json').then(v => v.json()))
 
 init({
   fallbackLocale: 'en',
   initialLocale: getLocaleFromNavigator(),
 }).then(() => {
   const app = new App({ target: document.body })
+  version.set(__SNOWPACK_ENV__.VERSION)
 
   if (import.meta.hot) {
     import.meta.hot.accept()
